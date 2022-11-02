c275512676f84b8bd8ebf80f1d1af682bc39a26d
Thrilok kumar <thrilok2000@gmail.com>
2022-11-03 02:28:20 +0530

    added title

--- contracts/mainnet/test/iERC20.sol
+++ contracts/mainnet/test/iERC20.sol
@@ -1,6 +1,8 @@
 
 pragma solidity ^0.8.11;
 
+// @title Polygon Lite vault
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
