db6b67220ed5b66cbb755baa61bb65c4acffaa3f
Thrilok kumar <thrilok2000@gmail.com>
2022-11-17 02:47:01 +0530

    added admin function + require statements

--- contracts/liteBridge/polygon/LitePolygonBridge.sol
+++ contracts/liteBridge/polygon/LitePolygonBridge.sol
@@ -60,11 +60,58 @@ abstract contract FxBaseChildTunnel is VariablesV1 {
 }
 
 abstract contract AdminModule is FxBaseChildTunnel {
-    // Add function to add RootToChain Mapping
-    // Add function to add rebalancer Mapping
+    event LogToggleChildToRootVaultMap(
+        address indexed rootVault,
+        address indexed childVault,
+        bool indexed add
+    );
 
-    function setRebalancer(address _account, bool flag) external /* onlyOwner */ {
-        rebalancer[_account] = flag;
+    event LogToggleRebalancer(
+        address indexed rebalancer,
+        bool indexed add
+    );
+
+    modifier onlyRebalancer() {
+        require(rebalancer[msg.sender], "LBP: not a rebalancer");
+        _;
+    }
+
+    function toggleChildToRootVaultMap(
+        address rootVault,
+        address childVault,
+        bool add
+    ) public /* onlyOwner */ {
+        if (add) {
+            require(childToRootVault[rootVault] == address(0), "LBP:[toggleChildToRootVaultMap]:: child to root Mapping already added");
+            childToRootVault[rootVault] = childVault;
+        } else {
+            require(childToRootVault[rootVault] != address(0), "LBP:[toggleChildToRootVaultMap]:: child to root Mapping not added");
+            delete childToRootVault[rootVault];
+        }
+
+        emit LogToggleChildToRootVaultMap(
+            rootVault,
+            childVault,
+            add
+        );
+    }
+
+    function toggleRebalancer(
+        address rebalancerAddress,
+        bool add
+    ) public /* onlyOwner */ {
+        if (add) {
+            require(!rebalancer[rebalancerAddress], "LBP:[toggleRebalancer]:: rebalancerAddress already enabled");
+            rebalancer[rebalancerAddress] = add;
+        } else {
+            require(rebalancer[rebalancerAddress], "LBP:[toggleRebalancer]::rebalancerAddress not enabled");
+            rebalancer[rebalancerAddress] = add;
+        }
+
+        emit LogToggleRebalancer(
+            rebalancerAddress,
+            add
+        );
     }
 
     constructor(address _fxChild) FxBaseChildTunnel(_fxChild) {}
@@ -72,10 +119,6 @@ abstract contract AdminModule is FxBaseChildTunnel {
 
 contract LitePolygonBridge is AdminModule {
     using SafeERC20 for IERC20;
-
-    // function setRebalancer(address _account, bool flag) external onlyOwner {
-    //     rebalancer[_account] = flag;
-    // }
 
     receive() external payable {}
 
@@ -95,7 +138,7 @@ contract LitePolygonBridge is AdminModule {
         uint256 stateId,
         address sender,
         bytes memory data
-    ) internal override /* validateSender(sender) */ {
+    ) internal override validateSender(sender) {
         (bytes32 key_, uint256 bridgeNonce_, bytes memory encodedRootData_) = abi.decode(data, (bytes32, uint256, bytes));
         emit LogMessageReceived(stateId, bridgeNonce_, key_);
 
@@ -109,13 +152,14 @@ contract LitePolygonBridge is AdminModule {
     function processUpdateExchangePrice(uint256[] memory bridgeNonces) public {
         for (uint256 i = 0; i < bridgeNonces.length; i++) {
             StateData memory stateData_ = bridgeNonceToData[bridgeNonces[i]];
-            require(stateData_.isExecuted == 0, "already-updated");
-            // require
+
+            require(stateData_.isExecuted == 0, "LBP:[processUpdateExchangePrice]:: already executed");
+
             if(stateData_.key == UPDATE_EXCHANGE_PRICE_SINGLE) {
                 ExchangePriceData memory exchangePriceData_ = abi.decode(stateData_.data, (ExchangePriceData));
                 IiTokenVaultPolygon(exchangePriceData_.childVault).updateExchangePrice(exchangePriceData_.exchangePrice);
                 emit LogUpdatedExchangePrice(bridgeNonces[i], exchangePriceData_.childVault, exchangePriceData_.exchangePrice);
-            } else if (false) { //
+            } else if (stateData_.key == UPDATE_EXCHANGE_PRICE_MULTI) { //
                 ExchangePriceData[] memory exchangePriceDatas_ = abi.decode(stateData_.data, (ExchangePriceData[]));
                 uint256 length_ = exchangePriceDatas_.length;
                 for (uint256 j = 0; j < length_; j++) {
@@ -123,6 +167,8 @@ contract LitePolygonBridge is AdminModule {
                     IiTokenVaultPolygon(exchangePriceDatas_[j].childVault).updateExchangePrice(exchangePriceDatas_[j].exchangePrice);
                     emit LogUpdatedExchangePrice(bridgeNonces[i], exchangePriceDatas_[j].childVault, exchangePriceDatas_[j].exchangePrice);
                 }
+            } else {
+                revert("LBP:[processUpdateExchangePrice]:: not update exchange price key");
             }
             bridgeNonceToData[bridgeNonces[i]].isExecuted = 1;
         }
@@ -141,11 +187,11 @@ contract LitePolygonBridge is AdminModule {
         address token,
         uint256 amount
     ) public /* OnlyRebalancer */ {
+        // TODO process message
 
         IERC20(token).safeApprove(vault, amount);
         IiTokenVaultPolygon(vault).fromMainnet(amount);
 
-        // Emit event
         emit LogFromMainnet(
             vault,
             vault,
